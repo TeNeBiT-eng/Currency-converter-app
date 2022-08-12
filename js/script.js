7e287707e97b7845dc03d2e467dd359ec9c09fab
Tomisin Babalola <ademolababalola2003@gmail.com>
2022-08-12 16:14:55 +0100

    getting exchange rate

---
+++ js/script.js
@@ -0,0 +1,62 @@
+const dropList = document.querySelectorAll(".drop-list select");
+const fromCurrency = document.querySelector(".from select");
+const toCurrency = document.querySelector(".to select");
+const getButton = document.querySelector("form button");
+
+for (let i = 0; i < dropList.length; i++) {
+  for (currency_code in country_code) {
+    // selecting USD and NGN by default
+    let selected;
+    if (i == 0) {
+      selected = currency_code == "USD" ? "selected" : "";
+    } else if (i == 1) {
+      selected = currency_code == "NGN" ? "selected" : "";
+    }
+    // creating option tag by passing currency code with text and value
+    let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+    // inserting options tag inside select tag
+    dropList[i].insertAdjacentHTML("beforeend", optionTag);
+  }
+  dropList[i].addEventListener("change", (e) => {
+    loadFlag(e.target); // calling load flag with passing target element as an argument
+  });
+}
+
+function loadFlag(element) {
+  for (code in country_code) {
+    if (code == element.value) {
+      // if currency code of country is equal to option value
+      let imgTag = element.parentElement.querySelector("img");
+      imgTag.src = `https://countryflagsapi.com/png/${country_code[code]}`
+    }
+  }
+}
+
+window.addEventListener("onload", () => {
+  getExchangeRate();
+});
+
+getButton.addEventListener("click", (e) => {
+  e.preventDefault(); // preventing form from submitting
+  getExchangeRate();
+});
+
+function getExchangeRate() {
+  const amount = document.querySelector(".amount input");
+  const exchangeRateTxt = document.querySelector(".exchange-rate");
+  let amountVal = amount.value;
+  // if user doesn't input any value OR inputs 0, then we get 1 by default
+  if (amountVal == "" || amountVal == "0") {
+    amount.value = "1";
+    amountVal = 1;
+  }
+  exchangeRateTxt.innerText = "Getting exchange rate...";
+  let url = `https://v6.exchangerate-api.com/v6/f40b90e8e0fe9ec60f229262/latest/${fromCurrency.value}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((result) => {
+      let exchangeRate = result.conversion_rates[toCurrency.value];
+      let totalExchangeRate = (amountVal * exchangeRate).toFixed(2);
+      exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}`;
+    });
+}
